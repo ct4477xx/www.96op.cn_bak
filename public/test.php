f1fa574f339ad8a5b8ffc79e9469a0bc254b94f1
ct4477xx <jiajun_520@126.com>
2020-03-09 22:12:15 +0800

    ..

--- public/test.php
+++ public/test.php
@@ -2,15 +2,11 @@
 
 
 use http\Env\Request;
-use Illuminate\Support\Facades\DB;
 
 function test($id)
 {
     echo $id;
 }
 
-$rs = DB::select('select * from user');
-return $rs;
 
-
-test(''234);
+test('now');
