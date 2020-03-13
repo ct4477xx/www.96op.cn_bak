68a84bfe5ea2bb8720165779143f90f0acf7cdf0
ct4477xx <jiajun_520@126.com>
2020-03-13 11:27:56 +0800

    laravel 6.0

--- config/filesystems.php
+++ config/filesystems.php
@@ -37,7 +37,7 @@ return [
     | may even configure multiple disks of the same driver. Defaults have
     | been setup for each driver as an example of the required options.
     |
-    | Supported Drivers: "local", "ftp", "s3", "rackspace"
+    | Supported Drivers: "local", "ftp", "sftp", "s3"
     |
     */
 
@@ -45,7 +45,7 @@ return [
 
         'local' => [
             'driver' => 'local',
-            'root' => public_path(),
+            'root' => storage_path('app'),
         ],
 
         'public' => [
@@ -61,6 +61,7 @@ return [
             'secret' => env('AWS_SECRET_ACCESS_KEY'),
             'region' => env('AWS_DEFAULT_REGION'),
             'bucket' => env('AWS_BUCKET'),
+            'url' => env('AWS_URL'),
         ],
 
     ],
--- routes/web.php
+++ routes/web.php
@@ -11,59 +11,6 @@
 |
 */
 
-Route::get('/login',function(){
-    return 'login page';
+Route::get('/', function () {
+    return view('welcome');
 });
-//前台
-Route::group([],function(){
-
-	Route::get('/',function(){
-		return view('welcome');
-	});
-
-	Route::get('/admin',function(){
-		return '这是一个牛逼胡后台';
-	})->name('ht')->middleware('login');
-
-	Route::get('/home',function(){
-		return '<a href='.route('ht').'>后台</a>';
-	});
-
-	Route::get('/update',function(){
-        echo 'update';
-    })->middleware('login');
-
-	Route::get('/delete',function(){
-        echo 'delete';
-    })->middleware('login');
-
-	Route::get('/404}',function(){
-		if(empty($_GET['id'])){
-			abort(404);
-		}
-	});
-});
-
-
-
-//后台
-Route::get('/user/add','UserController@add');
-Route::post('/user/insert','UserController@insert');
-
-
-//Api
-Route::get('/user/{id}','UserController@show')->name('user.show')->where('id','\d+');
-Route::get('/user/index','UserController@index')->middleware('login');
-
-
-//资源控制器
-Route::resource('tiezi','TieziController');
-Route::post('/upload','TieziController@upload');
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
-Route::get('/set','HomeController@set');
-Route::get('/get','HomeController@get');
-
-Route::get('/select','DBController@select');
-Route::get('/model','DBController@model');
